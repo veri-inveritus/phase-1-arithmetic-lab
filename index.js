0d559862fdeb751a91ef13b18d1606a3c589e009
David <yidavidh@outlook.com>
2023-03-18 10:58:51 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//Multiply
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply);
+
+//Generate Random integer greater than 0
+const random = parseInt(Math.random(1,2000)*2000);
+console.log(random);
+
+
+//Calculates an equation wirh a reimader of 4
+let num3 = 14
+let num4 = 5
+
+const  mod = num3 % num4;
+console.log(mod);
+
+//Returns 20 as highest number in a set
+let numbers = [5, 10, 15, 20, 8, 3];
+let max = Math.max(...numbers);
+console.log(max);
